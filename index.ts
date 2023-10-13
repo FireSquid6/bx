938454e6504f75ac7141a6b54a36c7be669c33ef
firesquid <jdeiss06@gmail.com>
2023-10-12 23:03:07 -0500

    create package

--- index.ts
+++ index.ts
@@ -1,1 +1,20 @@
-console.log("Hello via Bun!");
+interface CommandResult {
+  stdout: string;
+  stderr: string;
+  status: boolean;
+  exitCode: number;
+}
+
+export default function $(command: string): CommandResult {
+  const command_list = command.split(" ");
+  const res = Bun.spawnSync(command_list, {
+    stdio: ["inherit", "pipe", "pipe"],
+  });
+
+  return {
+    stdout: res.stdout.toString(),
+    stderr: res.stderr.toString(),
+    status: res.success,
+    exitCode: res.exitCode,
+  };
+}
---
+++ tests/playground.ts
@@ -0,0 +1,10 @@
+import $ from "../";
+
+let myvar = "hello world";
+const res = $(`echo ${myvar}`);
+
+console.log(res.stdout);
+
+Bun.spawnSync(["echo", "hello world"], {
+  stdio: ["inherit", "inherit", "inherit"],
+});
