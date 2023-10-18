086f0de75726dba610c45f12781ea7a5ab42defc
firesquid <jdeiss06@gmail.com>
2023-10-18 18:08:20 -0500

    use inherited stdio

--- index.ts
+++ index.ts
@@ -24,12 +24,12 @@ export default function $(
 export function runCommand(command: string) {
   const command_list = command.split(" ");
   const res = Bun.spawnSync(command_list, {
-    stdio: ["inherit", "pipe", "pipe"],
+    stdio: ["inherit", "inherit", "inherit"],
   });
 
   return {
-    stdout: res.stdout.toString(),
-    stderr: res.stderr.toString(),
+    stdout: (res.stdout || "").toString(),
+    stderr: (res.stderr || "").toString(),
     status: res.success,
     exitCode: res.exitCode,
   };
